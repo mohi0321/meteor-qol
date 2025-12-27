27fe4069009aaa039c994cb0dffe8e2947eaac1f
mohi0321 <benmohi121@gmail.com>
2025-12-28 00:17:44 +0100

    initial commit

--- src/main/java/com/example/addon/modules/PlayerAlert.java
+++ src/main/java/com/example/addon/modules/PlayerAlert.java
@@ -1,7 +1,6 @@
 package com.example.addon.modules;
 
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.settings.Settings;
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.events.world.TickEvent;
@@ -16,15 +15,13 @@ public class PlayerAlert extends Module {
     private static final long ALERT_COOLDOWN = 5000; // 5 seconds between alerts per player
     private java.util.Map<String, Long> lastAlertTime = new java.util.HashMap<>();
 
-    private final Settings settings = new Settings();
-
-    private final BoolSetting enableTeleport = settings.getGeneral().add(new BoolSetting.Builder()
+    private final BoolSetting enableTeleport = settings.getDefaultCategory().add(new BoolSetting.Builder()
         .name("teleport-on-alert")
         .description("Automatically teleport to spawn when a player is detected.")
         .defaultValue(false)
         .build());
 
-    private final IntSetting teleportDelay = settings.getGeneral().add(new IntSetting.Builder()
+    private final IntSetting teleportDelay = settings.getDefaultCategory().add(new IntSetting.Builder()
         .name("teleport-delay")
         .description("Delay in seconds before teleporting (after player detected).")
         .defaultValue(5)
