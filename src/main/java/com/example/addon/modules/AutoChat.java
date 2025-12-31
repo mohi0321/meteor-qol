64cf61a1aefba9c69d21875b8c2fc57ae7e392b6
mohi0321 <benmohi121@gmail.com>
2025-12-31 21:15:39 +0100

    initial commit

--- src/main/java/com/example/addon/modules/AutoChat.java
+++ src/main/java/com/example/addon/modules/AutoChat.java
@@ -1,4 +1,4 @@
-package com.example.addon.modules;
+package com.example.addon.AddonTemplate;
 
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.settings.*;
