e991a618fc6c895eb96e675681e779c8be140433
mohi0321 <benmohi121@gmail.com>
2026-01-02 21:43:02 +0100

    debugging

--- src/main/java/com/example/addon/modules/AutoAuction.java
+++ src/main/java/com/example/addon/modules/AutoAuction.java
@@ -131,7 +131,7 @@ public class AutoAuction extends Module {
                 ChatUtils.info("Found item in slot: " + foundSlot);
 
                 // Pick up the item
-                InvUtils.click(foundSlot)
+                InvUtils.click(foundSlot);
 
                 timer = ONE_SECOND;
                 step = Step.PLACE_ITEM;
